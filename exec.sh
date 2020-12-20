2f1c0962d31f0e51739baf5f4c262335ed0ef934
José A. Galindo <jagalindo@us.es>
2020-12-20 18:05:35 +0100

    automatic commit with new file generated

--- exec.sh
+++ exec.sh
@@ -1,2 +1,2 @@
-docker run --rm -v /home/admin/Software/Developement/wordpress_crawler/data:/data wp_crawl
+#docker run --rm -v /home/admin/Software/Developement/wordpress_crawler/data:/data wp_crawl
 cd /home/admin/Software/Developement/wordpress_crawler;git add -A; git commit -m "automatic commit with new file generated"; git push
