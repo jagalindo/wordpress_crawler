d74e595cdc244af35a7ff4ac9989828a3be768a0
José A <malawito@gmail.com>
2020-12-20 17:31:36 +0100

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,1 +1,2 @@
 docker run --rm -v /home/egcuser/wordpress_crawler/data:/data wp_crawl 2> /dev/null
+#cd /home/egcuser/wordpress_crawler;git add -A; git commit -m "automatic commit with new file generated"; git push
